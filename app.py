d9ea9c086fc2f447eb9e49a37289a175b098b4cd
andrometocs <kumaran.kanniappan@andrometocs.com>
2022-02-03 14:07:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import seaborn as sns
+import os
+import streamlit as st
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
+
+def main():
+    """Common ML dataset explorer"""
+    st.title("Machine Learning Dataset Explorer")
+    st.subheader("Using streamlit app to explore Machine Learning Datasets")
+
+
+if __name__ == "__main__":
+    main()
